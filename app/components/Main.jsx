0528a20afd920eae2e7f65478841a2da05fe9293
Mahdieh Melati <m.melati@etick.local>
2018-09-22 11:33:00 +0330

    Add filter functionality

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,26 +1,25 @@
 var React = require('react');
+var uuid=require('node-uuid');
+
+
 var Search= require('Search');
 var TodoList=require('TodoList');
 var AddTodo= require('AddTodo');
-var uuid=require('node-uuid')
+var TodoAPI= require('TodoAPI');
+
 var Main=React.createClass({
   getInitialState: function()
   {
     return{
       showCompleted:false,
       searchText:'',
-      todos:
-      [
-        {
-          id: uuid(),
-          text:'Cooking'
-        },
-        {
-          id:uuid(),
-          text:'Playing Music'
-        }
-      ]
+      todos:TodoAPI.getTodos()
+
     };
+  },
+  componentDidUpdate: function()
+  {
+    TodoAPI.setTodos(this.state.todos);
   },
   handleAddTodo: function(text)
   {
@@ -29,10 +28,24 @@ var Main=React.createClass({
         ...this.state.todos,
         {
           id: uuid(),
-          text: text
+          text: text,
+          completed: false
         }
       ]
     });
+  },
+  handleToggle: function(id)
+  {
+    var updatedTodos=this.state.todos.map((todo)=>
+  {
+    if(todo.id===id)
+    {
+    todo.completed=! todo.completed;
+  }
+    return todo;
+  });
+
+    this.setState({todos: updatedTodos});
   },
   handleSearch: function(showCompleted,searchText)
   {
@@ -43,7 +56,9 @@ var Main=React.createClass({
   },
   render: function()
   {
-    var{todos}=this.state;
+    var{todos,showCompleted,searchText}=this.state;
+    var filteredTodos=TodoAPI.filterTodos(todos,showCompleted,searchText);
+
     return(
       <div>
         <div  className="page-title">
@@ -54,7 +69,7 @@ var Main=React.createClass({
         </div>
 
         <div>
-          <TodoList todos={todos}/>
+          <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
 
         </div>
         <div>
