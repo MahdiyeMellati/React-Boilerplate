f26454da3f6ca8568ecf191685c16c76eadefdbe
Mahdieh Melati <m.melati@etick.local>
2018-10-17 12:13:18 +0330

    refactor app to work with local storage

--- app/app.jsx
+++ app/app.jsx
@@ -5,11 +5,15 @@ var {Route,Router,IndexRoute,hashHistory}= require("react-router");
 var Main = require('Main');
 var actions= require('actions');
 var store= require('configureStore').configure();
-
+var TodoAPI= require('TodoAPI');
 store.subscribe(()=>
 {
-  console.log('new state : ',store.getState() );
+  var state= store.getState();
+  console.log('new state : ',state );
+  TodoAPI.setTodos(state.todos);
 });
+var initialTodos= TodoAPI.getTodos();
+store.dispatch(actions.addTodos(initialTodos));
 //Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
